732ba1a58f16530b642e7f4b91281cccb92729b5
Sergey <simpleadm@gmail.com>
2018-03-16 21:05:35 +0300

    Backport of MAGETWO-71697: Added test to check that street array will be converted to the string

--- app/code/Magento/Customer/Test/Unit/Model/Address/AbstractAddressTest.php
+++ app/code/Magento/Customer/Test/Unit/Model/Address/AbstractAddressTest.php
@@ -363,4 +363,27 @@ class AbstractAddressTest extends \PHPUnit_Framework_TestCase
             'validated' => [array_merge($data, ['country_id' => $countryId++]), true],
         ];
     }
+
+    /**
+     * @dataProvider getStreetFullDataProvider
+     */
+    public function testSetDataStreetAlwaysConvertedToString($expectedResult, $street)
+    {
+        $this->model->setData('street', $street);
+        $this->assertEquals($expectedResult, $this->model->getData('street'));
+    }
+
+    /**
+     * @return array
+     */
+    public function getStreetFullDataProvider()
+    {
+        return [
+            [null, null],
+            ['', []],
+            ["first line\nsecond line", ['first line', 'second line']],
+            ['single line', ['single line']],
+            ['single line', 'single line'],
+        ];
+    }
 }
